958a0ae6e8547c53d3cb00c8a8130db398920e62
Dima Safonchyk <safonchyk@ukr.net>
2025-02-22 18:40:22 +0200

    Створення бд за допомогою EF

---
+++ Infrastructure/Migrations/20250222161247_InitialCreate.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Create_DateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductVersionTypes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Version_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Version_price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductVersionTypes", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type_name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Company_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    User_count = table.Column<int>(type: "int", nullable: false),
+                    Contact_manager_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contact_manager_phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Contact_manager_email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductVersionTypeID = table.Column<int>(type: "int", nullable: false),
+                    Subscription_time = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Companies_ProductVersionTypes_ProductVersionTypeID",
+                        column: x => x.ProductVersionTypeID,
+                        principalTable: "ProductVersionTypes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyChats",
+                columns: table => new
+                {
+                    CompanyID = table.Column<int>(type: "int", nullable: false),
+                    ChatID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyChats", x => new { x.CompanyID, x.ChatID });
+                    table.ForeignKey(
+                        name: "FK_CompanyChats_Chats_ChatID",
+                        column: x => x.ChatID,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompanyChats_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Department_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Worker_count = table.Column<int>(type: "int", nullable: false),
+                    CompanyID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Departments_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Meets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Meet_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyID = table.Column<int>(type: "int", nullable: false),
+                    Meet_URL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Meet_DateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Meets_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    First_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Last_name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date_of_birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Phone_number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    E_mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserTypeID = table.Column<int>(type: "int", nullable: false),
+                    CompanyID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Companies_CompanyID",
+                        column: x => x.CompanyID,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_UserTypes_UserTypeID",
+                        column: x => x.UserTypeID,
+                        principalTable: "UserTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DepartmentChats",
+                columns: table => new
+                {
+                    DepartmentID = table.Column<int>(type: "int", nullable: false),
+                    ChatID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentChats", x => new { x.DepartmentID, x.ChatID });
+                    table.ForeignKey(
+                        name: "FK_DepartmentChats_Chats_ChatID",
+                        column: x => x.ChatID,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DepartmentChats_Departments_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MeetUsers",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    MeetID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MeetUsers", x => new { x.UserID, x.MeetID });
+                    table.ForeignKey(
+                        name: "FK_MeetUsers_Meets_MeetID",
+                        column: x => x.MeetID,
+                        principalTable: "Meets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MeetUsers_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChatID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Chats_ChatID",
+                        column: x => x.ChatID,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserChats",
+                columns: table => new
+                {
+                    UserID1 = table.Column<int>(type: "int", nullable: false),
+                    UserID2 = table.Column<int>(type: "int", nullable: false),
+                    ChatID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserChats", x => new { x.UserID1, x.UserID2 });
+                    table.ForeignKey(
+                        name: "FK_UserChats_Chats_ChatID",
+                        column: x => x.ChatID,
+                        principalTable: "Chats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserChats_Users_UserID1",
+                        column: x => x.UserID1,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserChats_Users_UserID2",
+                        column: x => x.UserID2,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserDepartments",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDepartments", x => new { x.UserID, x.DepartmentID });
+                    table.ForeignKey(
+                        name: "FK_UserDepartments_Departments_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserDepartments_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_ProductVersionTypeID",
+                table: "Companies",
+                column: "ProductVersionTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyChats_ChatID",
+                table: "CompanyChats",
+                column: "ChatID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentChats_ChatID",
+                table: "DepartmentChats",
+                column: "ChatID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_CompanyID",
+                table: "Departments",
+                column: "CompanyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Meets_CompanyID",
+                table: "Meets",
+                column: "CompanyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MeetUsers_MeetID",
+                table: "MeetUsers",
+                column: "MeetID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ChatID",
+                table: "Messages",
+                column: "ChatID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserID",
+                table: "Messages",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserChats_ChatID",
+                table: "UserChats",
+                column: "ChatID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserChats_UserID2",
+                table: "UserChats",
+                column: "UserID2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDepartments_DepartmentID",
+                table: "UserDepartments",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CompanyID",
+                table: "Users",
+                column: "CompanyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserTypeID",
+                table: "Users",
+                column: "UserTypeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyChats");
+
+            migrationBuilder.DropTable(
+                name: "DepartmentChats");
+
+            migrationBuilder.DropTable(
+                name: "MeetUsers");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "UserChats");
+
+            migrationBuilder.DropTable(
+                name: "UserDepartments");
+
+            migrationBuilder.DropTable(
+                name: "Meets");
+
+            migrationBuilder.DropTable(
+                name: "Chats");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "UserTypes");
+
+            migrationBuilder.DropTable(
+                name: "ProductVersionTypes");
+        }
+    }
+}
