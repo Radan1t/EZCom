6bea6e672b3f7b2d7d3fc139ab4f5734f3531076
Dima Safonchyk <safonchyk@ukr.net>
2025-04-13 18:51:37 +0300

    Add direct Chat

---
+++ EZCom/Forms/Chat/ChatFormDep.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.DTO;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EZCom.Forms.Chat
+{
+    public partial class ChatFormDep : Form
+    {
+        UserDTO _userDTO;
+        int _chatid;
+        public ChatFormDep(UserDTO userDTO, int chatid)
+        {
+            this._userDTO = userDTO;
+            _chatid = chatid;
+            InitializeComponent();
+        }
+    }
+}
---
+++ EZCom/Forms/Chat/ChatFormPrivate.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.DTO;
+using Application.Interfaces.Services;
+using EZCom.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EZCom.Forms.Chat
+{
+    public partial class ChatFormPrivate : Form
+    {
+        UserDTO _userDTO;
+        int _chatid;
+        IChatService _chatService;
+        public ChatFormPrivate(UserDTO userDTO, int chatid)
+        {
+            this._userDTO = userDTO;
+            _chatid= chatid;
+            _chatService = Program.ServiceProvider.GetRequiredService<IChatService>();
+            InitializeComponent();
+            GetUsers();
+            
+        }
+        public async void GetUsers()
+        {
+            var companion = await _chatService.GetCompanionInPrivateChatAsync(_chatid, _userDTO.Id);
+
+            if (companion != null)
+            {
+                label1.Text = $"{companion.First_name} {companion.Last_name}";
+            }
+            else
+            {
+                label1.Text = "Співрозмовника не знайдено.";
+            }
+        }
+
+
+
+
+    }
+}
